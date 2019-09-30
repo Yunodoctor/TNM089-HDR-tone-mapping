8da81c7af6b70215805653c89b606c31cea0a18a
Sandmountain <sandtain@gmail.com>
2019-09-30 15:24:10 +0200

    Added comments to code

--- tonemap.m
+++ tonemap.m
@@ -7,19 +7,22 @@ n = size(Enorm3,1)*size(Enorm3,2);
 Lavg = exp((1/n)*sumL);
 a = 0.18;
 T = (a/Lavg)*L1;
+
 Tmax2 = max(T(:))*max(T(:));
 b = 1+ T/Tmax2;
 % Ltone = T.*b/(1+T);
 Ltone = zeros(682,1023);
+
+
 for p=1:682
     for h=1:1023
         Ltone(p,h) = (T(p,h)*b(p,h))/(1+T(p,h));
     end
 end
 
-M = zeros(480,640);
-for k1 = 1:480
-    for k2 = 1:640
+M = zeros(682,1023);
+for k1 = 1:682
+    for k2 = 1:1023
         M(k1,k2) = Ltone(k1,k2)/L1(k1,k2);
     end
 end
